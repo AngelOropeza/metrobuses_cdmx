226aff13f17dc18efa31a78e0e40143aa77ce8d1
angel.oropeza <angel.oropeza@rappi.com>
2022-07-10 22:56:23 -0500

    del(mock data)

--- app/migrations/V1.1__initial_migrations.sql
+++ app/migrations/V1.1__initial_migrations.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION unaccent;
+
 CREATE TABLE IF NOT EXISTS metrobuses (
     id SERIAL PRIMARY KEY,
     date_updated TIMESTAMP NOT NULL,
@@ -15,9 +17,3 @@ CREATE TABLE IF NOT EXISTS metrobuses (
     trip_route_id INT,
     alcaldia VARCHAR(80)
 );
-
-insert into metrobuses (id, date_updated, vehicle_id, vehicle_label, vehicle_current_status, position_latitude, position_longitude, geographic_point, position_speed, position_odometer, trip_schedule_relationship, trip_id, trip_start_date, trip_route_id, alcaldia) values (1, NOW()::timestamp, 1, 1, true, 437, 475, 'Goldenrod', 1, 1, 1, 1, NOW()::timestamp, 1, 'JumpXS');
-insert into metrobuses (id, date_updated, vehicle_id, vehicle_label, vehicle_current_status, position_latitude, position_longitude, geographic_point, position_speed, position_odometer, trip_schedule_relationship, trip_id, trip_start_date, trip_route_id, alcaldia) values (2, NOW()::timestamp, 2, 2, true, 393, 977, 'Pink', 2, 2, 2, 2, NOW()::timestamp, 2, 'Mybuzz');
-insert into metrobuses (id, date_updated, vehicle_id, vehicle_label, vehicle_current_status, position_latitude, position_longitude, geographic_point, position_speed, position_odometer, trip_schedule_relationship, trip_id, trip_start_date, trip_route_id, alcaldia) values (3, NOW()::timestamp, 3, 3, true, 586, 258, 'Purple', 3, 3, 3, 3, NOW()::timestamp, 3, 'Centizu');
-insert into metrobuses (id, date_updated, vehicle_id, vehicle_label, vehicle_current_status, position_latitude, position_longitude, geographic_point, position_speed, position_odometer, trip_schedule_relationship, trip_id, trip_start_date, trip_route_id, alcaldia) values (4, NOW()::timestamp, 4, 4, false, 846, 355, 'Orange', 4, 4, 4, 4, NOW()::timestamp, 4, 'Flashdog');
-insert into metrobuses (id, date_updated, vehicle_id, vehicle_label, vehicle_current_status, position_latitude, position_longitude, geographic_point, position_speed, position_odometer, trip_schedule_relationship, trip_id, trip_start_date, trip_route_id, alcaldia) values (5, NOW()::timestamp, 5, 5, false, 553, 710, 'Puce', 5, 5, 5, 5, NOW()::timestamp, 5, 'Zoovu');
